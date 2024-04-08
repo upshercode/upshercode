4bc0b8eec952bacd05fc43321a6ba5635d29430b
UPSHERCODE TECNOLOGIES <166451874+upshercode@users.noreply.github.com>
2024-04-08 20:53:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+//? MUDA O TEMA DE DARK PARA LIGTH
+const initLightMode = () => {
+    document.querySelector('input[type="checkbox"]').addEventListener('change', () => { document.body.classList.toggle('light-mode') })}
+
+
+//? MOVIMENTA AS LETRAS 
+const initTypingAnimation = () => {
+
+    const span = document.querySelector('#sobre .banner .intro .title span')
+    const paragraph = document.querySelector('#sobre .banner .intro .title p')
+
+    const typingAnimation = (element) => {
+
+        if (element == span) {
+            element.innerHTML = 'Hércules Chaves Andrade'
+            const textToArray = element.innerHTML.split('')
+            element.innerHTML = ''
+
+            textToArray.forEach((item, index) => {
+                setTimeout(() => element.innerHTML += item, 150 * index)
+            })
+
+        } else {
+            element.innerHTML = 'Desenvolvedor Full-stack'
+            const textToArray = element.innerHTML.split('')
+            element.innerHTML = ''
+
+            textToArray.forEach((item, index) => {
+                setTimeout(() => element.innerHTML += item, 45 * index)
+            })
+        }
+    }
+
+    typingAnimation(span)
+
+   
+    setTimeout(() => typingAnimation(paragraph), 3500)
+}
+
+initLightMode()
+initTypingAnimation()
